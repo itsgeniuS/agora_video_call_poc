c45b48379acc0e1fec90de8940a3b02845331c5a
Thulasirajan <iamgeniuskid7@gmail.com>
2024-10-11 01:50:37 +0700

    Initial commit

---
+++ lib/utils/AppID.dart
@@ -0,0 +1,8 @@
+var appID = '2bc15bc7d1534923bf17e22fa3fcc982';
+
+// Fill in the app ID obtained from the Agora console
+const appId = "2bc15bc7d1534923bf17e22fa3fcc982";
+// Fill in the temporary token generated from Agora Console
+const token = "<-- Insert token -->";
+// Fill in the channel name you used to generate the token
+const channel = "<-- Insert channel name -->";
